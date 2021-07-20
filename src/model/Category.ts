696822b8c15aa02b52d254da62b1044930abddce
ElsonSTM <engelp@gmail.com>
2021-07-19 23:16:50 -0300

    Json -> Categories

---
+++ src/model/Category.ts
@@ -0,0 +1,16 @@
+import {v4 as uuidv4} from 'uuid';
+class Category {
+
+    id?: string; //?=infomação opcional
+    name: string;
+    description: string;
+    created_at: Date;
+
+    constructor() {
+        if(!this.id){//Se o não tiver nenhum id, se um novo objeto que esta sendo criado, crie o cadastro
+            this.id = uuidv4();
+        } 
+    }
+}
+
+export {Category}
