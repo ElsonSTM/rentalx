affebdad7dfeb30db5ef41d0b390594766197326
ElsonSTM <engelp@gmail.com>
2021-07-19 22:06:29 -0300

    Create -> Categories

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
 import express = require("express");
+import { categoriesRoutes } from "./routes/categories.routes";
 
 const app = express();
 
+app.use(express.json());
 
-app.get("/", (req,res) => {
-    return res.json({message: "Hello World"})
-});
+app.use(categoriesRoutes);
 
 app.listen(3333, () => console.log("Server is rumming!"));
